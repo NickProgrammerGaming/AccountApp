f537cbfdfdab83bc4058f61632e71ec4751f4b46
Nick <64863561+NickProgrammerGaming@users.noreply.github.com>
2023-04-09 19:59:39 +0300

    Initial commit

---
+++ app/src/main/java/janes/romanes/accountapp/HomeActivity.java
@@ -0,0 +1,20 @@
+package janes.romanes.accountapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+        return;
+    }
+}
+
